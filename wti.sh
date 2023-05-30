981986d1254126b37020717d801b99d23531c98b
Pierre Santamaria <pierre.santamaria@epita.fr>
2023-05-30 22:09:57 +0200

    good script

--- wti.sh
+++ wti.sh
@@ -9,16 +9,16 @@ if [ -z $word ]; then
     exit 1
 fi
 if [ -z $index ]; then
-    #random between 0 and 9
-    index=$(($RANDOM % 10))
+    #random between 1 and 10
+    index=$(($RANDOM % 10 + 1))
 fi
 
-word="echo #wallpaper $word | sed 's/ /+/g'"
+word=$(echo "%23wallpaper $word" | sed 's/ /+/g')
 
 # Get image url
-IMAGE_LINK=$((curl -s -A "Mozilla/5.0" -L deviantart.com/search?q=$word | sed 's/</\n</g' | grep '<a data-hook="deviation_link"' | sed -E 's/^.*href=\"([^\"]+)\".*$/\1/mg | head -n $index | tail -n 1'))
+IMAGE_LINK=$(curl -s -L -A 'Mozilla/5.0' deviantart.com/search?q=$word | sed 's/</\n</g' | grep '<a data-hook="deviation_link"' | sed -E 's/^.*href="([^\"]+)".*$/\1/mg' | head -n $index | tail -n 1)
 
 # Get image
-REAL_IMAGE_LINK=$((curl -s -A "Mozilla/5.0" -L $IMAGE_LINK | sed 's/</\n</g' | grep '<img alt="' | sed -E 's/^.*src=\"([^\"]+)\".*$/\1/mg' | head -n 1))
-wget --user-agent "Mozilla/5.0" -O wallpaper.jpg $REAL_IMAGE_LINK
+REAL_IMAGE_LINK=$(curl -s -A "Mozilla/5.0" -L $IMAGE_LINK | sed 's/</\n</g' | grep '<img alt="' | sed -E 's/^.*src=\"([^\"]+)\".*$/\1/mg' | head -n 1)
+wget --user-agent "Mozilla/5.0" -qO wallpaper.jpg $REAL_IMAGE_LINK
 feh --bg-scale wallpaper.jpg
