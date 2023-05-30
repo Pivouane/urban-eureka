e2f46101f1c9c848de991904452bbb43bb2e05fb
Pierre DANTAMARIA <pierre.santamaria@epita.fr>
2023-05-30 18:36:10 +0200

    true background script

--- dotd.sh
+++ dotd.sh
@@ -1,18 +1,17 @@
 #!/bin/sh
-# Script gets motd from github and downloads an image related from the internet
+# Script gets motd from github
 
 #motd url
 url="https://raw.githubusercontent.com/Pivouane/urban-eureka/main/motd.txt"
 
 # get number of lines in motd
-num_lines=$(curl -s $url | wc -l)
-
+num_lines=$(($(curl -s -H 'Cache-Control: no-cache, no-store' $url | wc -l) + 1))
 
 # get days from time since epoch
 days=$(($(date +%s) / 86400))
 
 # get line number to print from days
-line_num=$(($days % $num_lines + 1))
+line_num=$(($days % $num_lines))
 
 # print line
 curl -s $url | sed -n "$line_num"p
---
+++ wti.sh
@@ -0,0 +1,24 @@
+#!/bin/sh
+# Script gets image from google with a word
+# Usage: wti.sh <word>
+# Example: wti.sh cat
+word=$1
+index=$2
+if [ -z $word ]; then
+    echo "Usage: wti.sh <word>"
+    exit 1
+fi
+if [ -z $index ]; then
+    #random between 0 and 9
+    index=$(($RANDOM % 10))
+fi
+
+word="#wallpaper $word"
+
+# Get image url
+IMAGE_LINK=$((curl -s -A "Mozilla/5.0" -L deviantart.com/search?q=$word | sed 's/</\n</g' | grep '<a data-hook="deviation_link"' | sed -E 's/^.*href=\"([^\"]+)\".*$/\1/mg | head -n $index | tail -n 1'))
+
+# Get image
+REAL_IMAGE_LINK=$((curl -s -A "Mozilla/5.0" -L $IMAGE_LINK | sed 's/</\n</g' | grep '<img alt="' | sed -E 's/^.*src=\"([^\"]+)\".*$/\1/mg' | head -n 1))
+wget --user-agent "Mozilla/5.0" -O wallpaper.jpg $REAL_IMAGE_LINK
+feh --bg-scale wallpaper.jpg
